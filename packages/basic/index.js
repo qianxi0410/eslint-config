e1dd7d5911f13f258a387059152ed86991e55c7e
qianxi0410 <894871277@qq.com>
2022-03-19 09:32:11 +0800

    refactor: semi always

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -100,7 +100,7 @@ module.exports = {
 		'import/no-absolute-path': 'off',
 
 		// Common
-		semi: ['error', 'never'],
+		semi: ['error', 'always'],
 		curly: ['error', 'multi-or-nest', 'consistent'],
 		quotes: ['error', 'double'],
 		'quote-props': ['error', 'consistent-as-needed'],
