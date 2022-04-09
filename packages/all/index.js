fe84f2e2f14bea3d6c1656ccf8ee5392b75c3315
qianxi0410 <894871277@qq.com>
2022-04-09 14:59:10 +0800

    feat: add prettier support

--- packages/all/index.js
+++ packages/all/index.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: ["@qianxi0410/eslint-config-vue", "@qianxi0410/eslint-config-react"],
+  extends: [
+    "@qianxi0410/eslint-config-vue",
+    "@qianxi0410/eslint-config-react",
+    "plugin:prettier/recommended",
+  ],
 };
